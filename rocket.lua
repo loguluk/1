9bf414bb2403667670fd3e0fa01e1e662884fca0
loguluk <loguluk@gmail.com>
2026-09-26 17:31:34 +0300

    Refactor thruster control messages and logic

--- rocket.lua
+++ rocket.lua
@@ -5,28 +5,15 @@ local thrusters = {
     ["Back-Right (7)"]  = peripheral.wrap("liquid_vector_thruster_7")
 }
 
-print("=== FORWARD VECTOR TEST ===")
-print("Setting all thrusters MAX FORWARD (Y = 1.0)...")
+print("=== SETTING ALL THRUSTERS FORWARD ===")
 
 for name, t in pairs(thrusters) do
     if t then
-        -- Set Y to 1.0 (Maximum Forward tilt)
         t.setVector(0, 1.0)
-        print("Set " .. name .. " -> FORWARD [0, 1.0]")
+        print("Done: " .. name .. " -> FORWARD [0, 1.0]")
     else
         print("ERROR: " .. name .. " NOT FOUND!")
     end
 end
 
-print("\nAll thrusters are now set to MAX FORWARD.")
-print("Check each engine with Wrench if needed!")
-print("Press ENTER to reset back to normal [0, 0]...")
-read()
-
-for name, t in pairs(thrusters) do
-    if t then
-        t.setVector(0, 0)
-    end
-end
-
-print("Reset complete! Thrusters back to [0, 0].")
+print("Finished! All thrusters are locked FORWARD.")
